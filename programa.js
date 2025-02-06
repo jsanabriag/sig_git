545f64b2e8af63d95c5eb776cb1cd6c640288f0f
Julian Sanabria <jsanabriag24@gmail.com>
2025-02-05 20:50:57 -0500

    v0.3

--- programa.js
+++ programa.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var map = L.map('map').setView([4.672338410879445, -74.1442758015399], 15);
+var map = L.map('map').setView([4.6733435692761365, -74.14749445216205], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-});
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
